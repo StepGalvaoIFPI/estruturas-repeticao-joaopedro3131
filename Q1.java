0f34b53031fcdd908533247a38c1d2a1e92b598f
joaopedro3131 <joaopedroabreu3030@gmail.com>
2025-04-22 19:12:16 -0300

    Q1.java

---
+++ Q1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+ public class Q1{
+  public static void main (String [] args){
+    Scanner entrada= new Scanner(System.in);
+    int velocidade= Integer.value0f(entrada.nextLine()); 
+
+    if(velocidade>80){
+     System.out.println(x:"vermelho");
+    }else{
+     System.out.println(x:"verde");
+    }
+   }
+  }
+  
